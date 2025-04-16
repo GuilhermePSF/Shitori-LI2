abd3a7cfb7b9cd97dca068bc8cd213b266128baa
GuilhermePSF <guilhermepsf23@gmail.com>
2025-04-16 10:36:30 +0100

    feat: implement undo command using a stack update header file

--- src/board.h
+++ src/board.h
@@ -1,17 +1,26 @@
-#ifndef TABULEIRO_H
-#define TABULEIRO_H
+#ifndef BOARD_H
+#define BOARD_H
 
 #define MAX_ROWS 26
 #define MAX_COLS 126
+#define MAX_HISTORY 100
 
-typedef struct {
+typedef struct
+{
     char grelha[MAX_ROWS][MAX_COLS];
     int linhas, colunas;
 } Tabuleiro;
 
-int carregarTabuleiro(Tabuleiro *tab, const char *ficheiro);
-int gravarTabuleiro(Tabuleiro *tab, const char *ficheiro);
-void modificarTabuleiro(Tabuleiro *tab, char acao, char *coord);
-void mostrarTabuleiro(Tabuleiro *tab);
+typedef struct
+{
+    Tabuleiro estados[MAX_HISTORY];
+    int topo;
+} Historico;
+
+void mostrarTabuleiro(const Tabuleiro *tab);
+int carregarTabuleiro(Tabuleiro *tab, Historico *hist, const char *ficheiro);
+int gravarTabuleiro(const Tabuleiro *tab, const char *ficheiro);
+void modificarTabuleiro(Tabuleiro *tab, Historico *hist, char acao, const char *coord);
+int desfazer(Historico *hist, Tabuleiro *atual);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
 #include "board.h"
 
-#define MAX_ROWS 26
-#define MAX_COLS 126
-#define BOARD_DIR "boards/"  // Diretório onde os arquivos de tabuleiro estão armazenados
-
 int main() {
     Tabuleiro tab;
+    Historico hist = { .topo = 0 };
     char cmd[256];
 
     while (1) {
@@ -19,18 +15,24 @@ int main() {
 
         if (cmd[0] == 'l') {
             char ficheiro[256];
-            if (sscanf(cmd, "l %s", ficheiro) == 1) carregarTabuleiro(&tab, ficheiro);
+            if (sscanf(cmd, "l %s", ficheiro) == 1)
+                carregarTabuleiro(&tab, &hist, ficheiro);
         } else if (cmd[0] == 'g') {
             char ficheiro[256];
-            if (sscanf(cmd, "g %s", ficheiro) == 1) gravarTabuleiro(&tab, ficheiro);
+            if (sscanf(cmd, "g %s", ficheiro) == 1)
+                gravarTabuleiro(&tab, ficheiro);
         } else if (cmd[0] == 'b' || cmd[0] == 'r') {
             char coord[50];
-            if (sscanf(cmd, "%*c %s", coord) == 1) modificarTabuleiro(&tab, cmd[0], coord);
+            if (sscanf(cmd, "%*c %s", coord) == 1)
+                modificarTabuleiro(&tab, &hist, cmd[0], coord);
+        } else if (cmd[0] == 'd') {
+            desfazer(&hist, &tab);
         } else if (cmd[0] == 's') {
             break;
         } else {
             printf("Comando inválido.\n");
         }
     }
+
     return 0;
 }
