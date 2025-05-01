45f744c5c17759914657e08d8123343b6cda9821
Lucas <lucasfariapinto@gmail.com>
2025-05-01 21:05:39 +0100

    fix : removed mostrarTabuleiro from the constituction of other functions

--- src/game.h
+++ src/game.h
@@ -4,15 +4,6 @@
 #include "board.h"
 #include "undo.h"
 
-#ifndef GAME_H
-#define GAME_H
-
-#include "board.h"
-#include "undo.h"
-#include <stdbool.h>
-
 bool modificarTabuleiro(Tabuleiro *tabAtual, Historico *hist, char cmd, const char *coord);
 
 #endif // GAME_H
-
-#endif
