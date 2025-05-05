fe8e309ed79615452a52553386757c0945912f1c
GuilhermePSF <guilhermepsf23@gmail.com>
2025-05-05 19:57:16 +0100

    fix: non applying ansii color code

--- src/game.c
+++ src/game.c
@@ -12,20 +12,20 @@ bool modificarTabuleiro(Tabuleiro *tabAtual, Historico *hist, char cmd, const ch
 
     if (linha < 0 || linha >= tabAtual->linhas || coluna < 0 || coluna >= tabAtual->colunas)
     {
-        printf("\\033[1;31m ⚠ Coordenada inválida. ⚠ \n\033[0m");
+        printf("\033[1;31m ⚠ Coordenada inválida. ⚠ \n\033[0m");
         return false;
     }
 
     if (!guardar_estado(hist, tabAtual))
     {
-        printf("\\033[1;31m ⚠ Erro ao guardar estado do tabuleiro. ⚠ \n\033[0m");
+        printf("\033[1;31m ⚠ Erro ao guardar estado do tabuleiro. ⚠ \n\033[0m");
         return false;
     }
     if (cmd == 'b') // Pintar de branco
     {
         if (tabAtual->grelha[linha][coluna] == '#')
         {
-            printf("\\033[1;31m ⚠ Erro: Não é possível pintar uma posição que já está riscada. ⚠ \n\033[0m");
+            printf("\033[1;31m ⚠ Erro: Não é possível pintar uma posição que já está riscada. ⚠ \n\033[0m");
             return false;
         }
         tabAtual->grelha[linha][coluna] = toupper(tabAtual->grelha[linha][coluna]);
@@ -34,14 +34,14 @@ bool modificarTabuleiro(Tabuleiro *tabAtual, Historico *hist, char cmd, const ch
     {
         if (isupper(tabAtual->grelha[linha][coluna]))
         {
-            printf("\\033[1;31m ⚠ Erro: Não é possível riscar uma posição que já está em branco. ⚠ \n\033[0m");
+            printf("\033[1;31m ⚠ Erro: Não é possível riscar uma posição que já está em branco. ⚠ \n\033[0m");
             return false;
         }
         tabAtual->grelha[linha][coluna] = '#';
     }
     else
     {
-        printf("\\033[1;31m ⚠ Comando inválido. ⚠ \n\033[0m");
+        printf("\033[1;31m ⚠ Comando inválido. ⚠ \n\033[0m");
         return false;
     }
 
