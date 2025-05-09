49d304b87a88ffc15dfaa0acbef88694de2d4c32
GuilhermePSF <guilhermepsf23@gmail.com>
2025-05-09 18:31:44 +0100

    chore: format

--- src/tip.h
+++ src/tip.h
@@ -4,7 +4,6 @@
 #include "board.h"
 #include "undo.h"
 
-
 bool tem_riscado_adjacente(const Tabuleiro *tab);
 bool tem_riscado_adjacente_coord(const Tabuleiro *tab, int linha, int coluna);
 bool existe_maiuscula_igual_na_linha_ou_coluna(Tabuleiro *tab, int linha, int coluna);
@@ -12,6 +11,5 @@ bool existe_igual_na_linha_ou_coluna(Tabuleiro *tab, int linha, int coluna);
 bool necessaria_para_conectividade(Tabuleiro *tabAtual, int linha, int coluna);
 void comando_a(Tabuleiro *tabAtual, Tabuleiro *tabIO, Historico *hist);
 void comando_A(Tabuleiro *tabAtual, Tabuleiro *tabIO, Historico *hist);
-    
 
 #endif
