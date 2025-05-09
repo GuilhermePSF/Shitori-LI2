75efa852f79becae86253c814ee2b604446e542e
GuilhermePSF <guilhermepsf23@gmail.com>
2025-05-09 18:12:12 +0100

    refactor: removed prints for centralization of io

--- src/undo.c
+++ src/undo.c
@@ -11,7 +11,6 @@ void inicializar_historico(Historico *hist)
     hist->topo = 0;
 }
 
-
 bool guardar_estado(Historico *hist, Tabuleiro *tabAtual)
 {
     if (hist == NULL || tabAtual == NULL)
@@ -24,23 +23,16 @@ bool guardar_estado(Historico *hist, Tabuleiro *tabAtual)
         hist->estados[hist->topo++] = *tabAtual;
         return true;
     }
-    else
-    {
-        printf("\033[1;31m ⚠ Aviso: Histórico cheio, não é possível guardar mais estados. ⚠ \n \n\033[0m");
-        return false;
-    }
+
+    return false;
 }
 
 bool desfazer(Historico *hist, Tabuleiro *tabAtual, Tabuleiro *tabIO, const char *coord)
 {
+    if (hist->topo == 0)
+        return false;
     if (coord == NULL)
     {
-        if (hist->topo == 0)
-        {
-            printf("\033[1;92m ✓ Não há movimentos para desfazer.  ✓\n\n\033[0m");
-            return false;
-        }
-
         hist->topo--;
         int i;
         for (i = 0; i < hist->estados[hist->topo].linhas; i++)
@@ -53,20 +45,8 @@ bool desfazer(Historico *hist, Tabuleiro *tabAtual, Tabuleiro *tabIO, const char
         return true;
     }
 
-    if (strlen(coord) < 2 || !isalpha(coord[0]) || !isdigit(coord[1]))
-    {
-        printf("\033[1;31m ⚠ Coordenada inválida. ⚠ \n \n\033[0m");
-        return false;
-    }
-
     int col = tolower(coord[0]) - 'a';
     int row = atoi(coord + 1) - 1;
-
-    if (row < 0 || row >= tabAtual->linhas || col < 0 || col >= tabAtual->colunas)
-    {
-        printf("\033[1;31m ⚠ Coordenada fora dos limites. ⚠ \n \n\033[0m");
-        return false;
-    }
 
     if (tabAtual->grelha[row][col] != tabIO->grelha[row][col])
     {
@@ -77,6 +57,6 @@ bool desfazer(Historico *hist, Tabuleiro *tabAtual, Tabuleiro *tabIO, const char
         }
         return false;
     }
-    printf("\033[1;92m ✓ Não há modificacoes para desfazer.  ✓\n\n\033[0m");
+
     return false;
 }
